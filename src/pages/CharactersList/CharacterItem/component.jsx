9e44d0f76ecd19584790a4ff982972c2b28b0fb7
Masha <masha.sladky@gmail.com>
2023-03-10 13:15:20 +0200

    added request on form

--- src/pages/CharactersList/CharacterItem/component.jsx
+++ src/pages/CharactersList/CharacterItem/component.jsx
@@ -4,9 +4,9 @@ import '../../../scss/item.scss';
 
 export const CharacterItem = ({ name, id, image, species }) => 
   (
-    <li className='list__item'>
-        <img src={image} alt={name} className="list__item-img"/>
-        <h3 className="list__item-name">{name}</h3>
+    <li className="list__item">
+      <img src={image} alt={name} className="list__item-img"/>
+      <h3 className="list__item-name">{name}</h3>
       <p className="list__item-species">{species}</p>
     </li>
-);
+  );
--- src/pages/Form/component.jsx
+++ src/pages/Form/component.jsx
@@ -1,14 +1,19 @@
 import '../../scss/form.scss';
+import { generateApiUrl } from '../utils';
 
 export const Form = () => {
   const handleChange = e => {
-        
+    fetch(generateApiUrl(`character/?name=${e.target.value}`))
+      .then(response => response.json())
+      .then(data => {
+      // Process data as needed
+        console.log(data.results);
+      })
   };
 
   return (
     <form action="" className="search">
-      <input className="search__field" type="text" placeholder="Filter by name..." />
-            
+      <input className="search__field" type="text" placeholder="Filter by name..."  onChange={handleChange}/>           
     </form>
   );
 };
