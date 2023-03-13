2a883157d4a9bb9cdbdee6721f791ef79c351aa9
Masha <masha.sladky@gmail.com>
2023-03-13 17:13:12 +0200

    added useSearchParams hook

--- src/pages/Form/component.jsx
+++ src/pages/Form/component.jsx
@@ -1,24 +1,37 @@
 import {connect} from 'react-redux';
+import { useSearchParams } from 'react-router-dom';
 
 import '../../scss/form.scss';
 import { generateApiUrl } from '../utils';
 import {selectSetSearchQuery, selectSearchedCharacters} from '../../store/characters/selectors';
 import { setSearchQuery, setSearchedCharacters} from '../../store/characters/actions';
 
-const Form = ({setSearchQuery, setSearchedCharacters}) => {
+const Form = ({setSearchQuery, setSearchedCharacters, searchQuery}) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
   const handleChange = e => {
-    e.preventDefault();
+    const search = {
+      character: e.target.value,
+    };
+    setSearchParams(search, { replace: true });
+
     setSearchQuery(e.target.value);
+    
     fetch(generateApiUrl(`character/?name=${e.target.value}`))
       .then(response => response.json())
       .then(data => {
         setSearchedCharacters(data.results);
-      });  
+      });
   };
 
   return (
     <form action="" className="search">
-      <input className="search__field" type="text" placeholder="Filter by name..."  onChange={handleChange}/>           
+      <input 
+        className="search__field" 
+        type="text" 
+        placeholder="Filter by name..."  
+        onChange={handleChange} 
+        value={searchQuery}/>           
     </form>
   );
 };
