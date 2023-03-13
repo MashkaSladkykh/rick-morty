8470bbfc08060caa036abf0bdaf405f27eba225b
Masha <masha.sladky@gmail.com>
2023-03-13 14:23:15 +0200

    fixed function

--- src/pages/utils.js
+++ src/pages/utils.js
@@ -2,4 +2,4 @@ import { API_URL } from './constants';
 
 export const generateApiUrl = path => `${API_URL}${path}`;
 
-export const sort = arr => arr.sort((a,b)=>  (a.namr > b.name) ? 1 : ((b.name > a.name) ? -1 : 0) );
+export const sort = arr => arr.sort((a,b)=>  (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
